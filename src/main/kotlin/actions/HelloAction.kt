deb0e99ec14d7abf92392ee602aa5cd0b69f7642
peng.li <nowindlee@gmail.com>
2019-08-01 22:31:54 +0800

    demo

--- src/main/kotlin/actions/HelloAction.kt
+++ src/main/kotlin/actions/HelloAction.kt
@@ -2,12 +2,14 @@ package actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.ui.Messages
 
 class HelloAction : AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
-        Messages.showInfoMessage("Hello from my plugin", "Hello")
+        val dir = Messages.showInputDialog("Input the dir your want to exclude", "Exclude dir", null);
+        ContentEntry
     }
 
 }
